4c2a9c91f0b41b5e4eba0c773e43e9f822df527e
OMEGA\Cristian <cristian.suciava@gmail.com>
2018-12-19 19:28:33 +0200

    x

--- Program.cs
+++ Program.cs
@@ -10,6 +10,30 @@ namespace DataTypes
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Enter a year:");
+            int year;
+            if (int.TryParse(Console.ReadLine(), out year) == false)
+            {
+                Console.WriteLine("Only numbers");
+            };
+
+            Console.WriteLine("Enter a month:");
+            int month;
+            if (int.TryParse(Console.ReadLine(), out month)==false)
+            {
+                Console.WriteLine("Only numbers");
+            };
+
+            Console.WriteLine("Enter a day:");
+            int day;
+            if (int.TryParse(Console.ReadLine(), out day) == false)
+            {
+                Console.WriteLine("Only numbers");
+            };
+
+            Console.ReadKey();
         }
+
     }
 }
